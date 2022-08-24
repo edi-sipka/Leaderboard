fd00598d634bf19e354d5de010695788c8e1fe24
edi-sipka@gmail.com <edisipka@gmail.com>
2022-08-24 17:39:43 +0100

    Fix the linters

--- src/api.js
+++ src/api.js
@@ -2,11 +2,11 @@ const list = document.querySelector('.scores');
 
 export const display = async () => {
   await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1WjHZNMPhymXrSt0JsIP/scores/'
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1WjHZNMPhymXrSt0JsIP/scores/',
   )
     .then((response) => response.json())
     .then((json) => {
-      list.innerHTML = ` `;
+      list.innerHTML = ' ';
       for (let i = 0; i < json.result.length; i += 1) {
         const item = document.createElement('li');
         item.classList.add('users');
@@ -20,22 +20,21 @@ export const addList = async () => {
   const user = document.getElementById('user').value;
   const score = document.getElementById('score').value;
   if (user === '' || score === '') {
-    const error = document.getElementById('error');
-    error.textContent = 'Please add user and score';
+    document.getElementById('error').innerHTML = 'Please add user and score';
   } else {
-    error.innerHTML = '';
+    document.getElementById('error').innerHTML = '';
     await fetch(
       'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/1WjHZNMPhymXrSt0JsIP/scores/',
       {
         method: 'POST',
         body: JSON.stringify({
           user,
-          score: score,
+          score,
         }),
         headers: {
           'Content-type': 'application/json; charset=UTF-8',
         },
-      }
+      },
     ).then((response) => response.json());
 
     display();
