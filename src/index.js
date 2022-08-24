dc76c76e89d2c0f6270e912158a6f6355c957352
edi-sipka@gmail.com <edisipka@gmail.com>
2022-08-24 16:58:51 +0100

    Add API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import { refresh, display, addList } from './api.js';
+
+const refreshing = document.getElementById('refresh');
+const submit = document.getElementById('submit');
+
+display();
+
+refreshing.addEventListener('click', refresh);
+submit.addEventListener('click', addList);
+
+console.log(refreshing);
